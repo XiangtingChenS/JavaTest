e570b24d2a93a90dde35ea08dc6a15f831c92b65
XiangTingChenS <jenny850214@gmail.com>
2018-02-21 16:26:50 +0800

    Update bbb.java

--- bbb.java
+++ bbb.java
@@ -1,23 +1,34 @@
-	@Override
-	public List<Enttity> countEntityByKeyword(Long projectId, IndexSearcher indexSearcher, ScoreDoc[] scoreDocs) {
+package com.gss.adm.core.service;
 
-		List<Enttity> entities = Lists.newArrayList();
-		Set<String> identifiers = Sets.newHashSet();
-		try {
-			for (int i = 0; i < scoreDocs.length; i++) {
-				Document hitDoc = indexSearcher.doc(scoreDocs[i].doc);
-				identifiers.add(hitDoc.get(ENTITY_IDENTIFIER));
-			}
-		} catch (Exception e) {
-			log.debug("indexSearcher get identifier column failure: {}", e);
-		}
 
-		if (CollectionUtils2.isNotEmpty(identifiers)) {
-			if (projectId != 0) {
-				entities = entityService.findByProjectIdAndIdentifiers(projectId, identifiers);
-			} else {
-				entities = entityService.findByIdentifiers(identifiers);
-			}
-		}
-		return entities;
-	}
+import java.util.List;
+import java.util.Map;
+
+import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.ScoreDoc;
+
+import com.gss.adm.core.condition.FullTextEntitySearchCondition;
+import com.gss.adm.core.form.FullTextSearchEntityForm;
+import com.gss.adm.core.model.Enttity;
+import com.gss.adm.core.model.FullTextSearchEntity;
+
+public interface FullTextSearchService {
+
+    void reindex();
+    
+    void sourceCodeReindex(long projectId);
+    
+    FullTextSearchEntity searchEntityByKeyword(FullTextEntitySearchCondition fullTextSearchForm);
+    
+    Map<Integer, Integer> getEntityDefinitionCountMap(FullTextEntitySearchCondition fullTextSearchForm);
+    
+    List<Enttity> countEntityByKeyword(Long projectId, IndexSearcher indexSearcher, ScoreDoc[] scoreDocs);
+    
+	FullTextSearchEntity searchEntityFromSourceCodeByKeyword(FullTextEntitySearchCondition condition, int dataBaseResultCount);
+
+	List<Enttity> searchEntityFromSourceCodeByKeywordAndProjectId(String methodName, Long projectId, int entitySize);
+	
+	List<Enttity> searchEntitiesByProjectIdAndDefinitionIdsAndKeywords(FullTextSearchEntityForm form);
+	
+	List<Enttity> searchEntitySourceCodeByKeywordAndProjectId(Long projectId, List<String> detailedTerms, int entitySize, int searchMode);
+}
